04d23e5c77946720464bbf88a18fea2e84d8fd74
diegomurillocorrea <diego.murillo@elaniin.com>
2021-11-03 00:26:10 -0600

    Employee images added

--- pages/home/Courses.jsx
+++ pages/home/Courses.jsx
@@ -5,11 +5,12 @@ const Courses = () => {
     return (
         <div className="bg-light-blue">
             <div className="py-10 px-5 md:px-8 grid grid-cols-1 md:grid-cols-2 md:gap-5 xl:grid-cols-3 xl:gap-8 xl:px-10 mx-auto max-w-screen-2xl">
-                {coursesData.map(({ nameCourse, description, img }) => (
+                {coursesData.map(({ nameCourse, description, img }, key) => (
                     <CourseCard
                         nameCourse={nameCourse}
                         description={description}
                         img={img}
+                        key={`course-card-image-${key}`}
                     />
                 ))}
             </div>
