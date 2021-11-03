ed1b3919e2bb671bf50859dc5303737fd5351410
diegomurillocorrea <diego.murillo@elaniin.com>
2021-11-02 23:01:44 -0600

    Footer component finished

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,18 @@
 const Footer = () => {
     return (
         <div>
-            
+            <div className="bg-black text-center py-5 md:py-10 flex flex-col md:flex-row-reverse md:space-y-0 md: justify-center items-center lg:justify-between font-righteous space-y-5 lg:px-20">
+                <div className="md:pl-10 lg:pl-0">
+                    <p className="text-white text-xs md:text-base lg:text-lg">
+                        Copyright &copy; {new Date().getFullYear()}. DevBook &reg; Todos los derechos reservados.
+                    </p>
+                </div>
+                <div>
+                    <p className="text-3xl md:text-5xl lg:text-6xl font-righteous">
+                        <span className="text-light-blue">{`<`}</span><span className="text-white">Dev</span><span className="text-light-blue">Book </span><span className="text-white">{`/>`}</span>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
--- pages/home/AboutUs.jsx
+++ pages/home/AboutUs.jsx
@@ -1,7 +1,9 @@
 const AboutUs = () => {
     return (
         <div>
-            
+            <div>
+                
+            </div>
         </div>
     );
 };
